971ce312197c9ce53665baf290b10e0c1a83e456
Max Hatfull <max.h@beam.org>
2025-12-31 19:38:08 +0000

    Cache direction light matrix

--- lib/engine/components/direction_light.rb
+++ lib/engine/components/direction_light.rb
@@ -4,13 +4,30 @@ module Engine::Components
   class DirectionLight < Engine::Component
     include Engine::MatrixHelpers
 
-    attr_accessor :colour, :cast_shadows, :shadow_distance, :shadow_layer_index
+    attr_reader :colour, :cast_shadows, :shadow_distance
+    attr_accessor :shadow_layer_index
 
     def initialize(colour: [1.0, 1.0, 1.0], cast_shadows: true, shadow_distance: 50.0)
       @colour = colour
       @cast_shadows = cast_shadows
       @shadow_distance = shadow_distance
       @shadow_layer_index = nil  # Set by RenderPipeline when rendering shadows
+      @cached_light_space_matrix = nil
+      @cache_key = nil
+    end
+
+    def colour=(value)
+      @colour = value
+    end
+
+    def cast_shadows=(value)
+      @cast_shadows = value
+      invalidate_cache
+    end
+
+    def shadow_distance=(value)
+      @shadow_distance = value
+      invalidate_cache
     end
 
     def start
@@ -26,6 +43,35 @@ module Engine::Components
     end
 
     def light_space_matrix
+      current_key = compute_cache_key
+      if @cached_light_space_matrix && @cache_key == current_key
+        return @cached_light_space_matrix
+      end
+
+      @cache_key = current_key
+      @cached_light_space_matrix = compute_light_space_matrix
+    end
+
+    def self.direction_lights
+      @direction_lights ||= []
+    end
+
+    private
+
+    def invalidate_cache
+      @cached_light_space_matrix = nil
+      @cache_key = nil
+    end
+
+    def compute_cache_key
+      camera_pos = Engine::Camera.instance&.position || Vector[0, 0, 0]
+      light_dir = direction
+      [camera_pos[0], camera_pos[1], camera_pos[2],
+       light_dir[0], light_dir[1], light_dir[2],
+       @shadow_distance]
+    end
+
+    def compute_light_space_matrix
       light_dir = direction
 
       # Center shadow frustum on the main camera's position
@@ -49,10 +95,6 @@ module Engine::Components
       proj_matrix = ortho(-half_size, half_size, -half_size, half_size, 0.1, @shadow_distance * 2)
 
       (proj_matrix * view_matrix).transpose
-    end
-
-    def self.direction_lights
-      @direction_lights ||= []
     end
   end
 end
