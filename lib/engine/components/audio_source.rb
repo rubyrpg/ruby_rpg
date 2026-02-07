9470ad437958dc07746a48986b2e6807bfc03183
Max Hatfull <max.h@beam.org>
2026-02-07 23:29:21 +0000

    Add reverb and delay to audio

--- lib/engine/components/audio_source.rb
+++ lib/engine/components/audio_source.rb
@@ -30,6 +30,30 @@ module Engine::Components
       @source.set_volume(volume)
     end
 
+    def pitch=(pitch)
+      @source.set_pitch(pitch)
+    end
+
+    def looping=(looping)
+      @source.set_looping(looping)
+    end
+
+    def duration
+      @clip.duration
+    end
+
+    def enable_reverb(enabled = true)
+      @source.enable_reverb(enabled)
+    end
+
+    def set_reverb(room_size: 0.5, damping: 0.3, wet: 0.3, dry: 1.0)
+      @source.set_reverb(room_size: room_size, damping: damping, wet: wet, dry: dry)
+    end
+
+    def add_delay_tap(time_ms:, volume:)
+      @source.add_delay_tap(time_ms: time_ms, volume: volume)
+    end
+
     def update(delta_time)
       camera = Engine::Camera.instance
       local_pos = camera.game_object.world_to_local_coordinate(game_object.pos)
