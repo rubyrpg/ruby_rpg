b457cd09a3d36f5d0544bee8c12f3ac2f036d3ee
MaxHatfull <71698066+MaxHatfull@users.noreply.github.com>
2026-02-08 14:03:07 +0000

    update native audio version (#70)
    * update native audio version
    
    * Add reverb and delay to audio
    
    * Tweak reverb setup

--- samples/cubes/components/sound_test.rb
+++ samples/cubes/components/sound_test.rb
@@ -5,6 +5,8 @@ module Cubes
     def start
       audio = game_object.components.find { |c| c.is_a?(Engine::Components::AudioSource) }
       audio&.play
+      audio&.looping = true
+      audio&.set_reverb(room_size: 0.8, damping: 0.2, wet: 0.5, dry: 1.0)
     end
   end
 end
